68319561e8130257fa3aa061101a30e86ba40639
Shahid Abdul Farhad <farhadshahidabdul@gmail.com>
2025-06-25 17:50:51 +0530

    Time: 2 ms (74.56%) | Memory: 61.1 MB (95.29%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,25 +1,21 @@
 class Solution {
     public int maxProfit(int[] prices) 
     {
-        // using Kadane's Algorithm (modified)
-        /** Every day is a selling day starting from index 1 
-        and we will see the minimum price in the previous subarray 
-        and pick the minimum which will be the 'BestBuy'.**/
+        int i, l=prices.length;
 
-        int i,j,n=prices.length;
-        int Maxprofit=0, BestBuy=prices[0];
-        for(i=1;i<n;i++)
+        int maxProfit=0;
+        int BestBuy=prices[0];
+
+        for(i=1;i<l;i++)
         {
-            if(prices[i]-BestBuy > Maxprofit)
-            {
-                Maxprofit=prices[i]-BestBuy;
-            }
-            else if(prices[i] < BestBuy)
-            {
-                BestBuy = prices[i];
-            }
             
+            if(prices[i]-BestBuy > 0)
+            {
+                maxProfit=Math.max(maxProfit, prices[i]-BestBuy);
+            }
+            BestBuy = Math.min(prices[i], BestBuy);
         }
-        return Maxprofit;
+
+        return maxProfit;
     }
 }
