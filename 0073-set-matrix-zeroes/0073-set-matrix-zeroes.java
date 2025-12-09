b01103be447c3f9772ff80f1d29ce43d590abe8b
Shahid Abdul Farhad <farhadshahidabdul@gmail.com>
2025-12-10 00:45:07 +0530

    Time: 1 ms (62.89%), Space: 47.4 MB (24.63%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,6 +2,63 @@ class Solution
 {
     public void setZeroes(int[][] matrix) 
     {
+        int i,j,col0=1;
+
+        // 1st part
+        for(i=0;i<matrix.length;i++)
+        {
+            for(j=0;j<matrix[0].length;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    if(j==0)
+                        col0=0;
+
+                    else
+                        matrix[0][j]=0;     // 1st row
+
+                    matrix[i][0]=0;         // 1st col
+                }
+            }
+        }
+
+        // 2nd part
+        for(i=matrix.length-1;i>=1;i--)
+        {
+            for(j=matrix[0].length-1;j>=1;j--)
+            {
+                if(matrix[i][0]==0 || matrix[0][j]==0)
+                {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        if(matrix[0][0]==0)
+        {
+            for(j=0;j<matrix[0].length;j++)
+            {
+                matrix[0][j]=0;
+            } 
+        }
+        
+
+        if(col0==0)
+        {
+            for(i=0;i<matrix.length;i++)
+            {
+               matrix[i][0]=0;
+            }
+        }
+
+    }
+        
+
+
+    /*  Better Approach - (NOT OPTIMAL) 
+        Space: O(n+m)
+        Time: O(n*m)
+    
         int i,j;
         List<Integer> rows = new ArrayList<Integer>();
         List<Integer> cols = new ArrayList<Integer>();
@@ -16,7 +73,6 @@ class Solution
                 }
             }
         }
-
         fillZeroes(rows,cols,matrix);
     }
 
@@ -38,4 +94,5 @@ class Solution
             }
         }
     }
+    */
 }
