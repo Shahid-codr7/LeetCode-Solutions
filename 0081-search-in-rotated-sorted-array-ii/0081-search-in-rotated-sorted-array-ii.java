a62a3b756ffd76eb074c47989504461f441367c2
Shahid Abdul Farhad <farhadshahidabdul@gmail.com>
2026-02-24 01:08:28 +0530

    Time: 0 ms (100%), Space: 45 MB (81.95%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,60 @@
+class Solution 
+{
+    public boolean search(int[] nums, int target) 
+    {
+        int n=nums.length;
+        if(n==0) return false;
+        if(n==1)
+        {
+            if(nums[0] == target)
+                return true;
+            else
+                return false;
+        }
+
+        int l=0,r=n-1,mid=0;
+        while(l<=r)
+        {
+            mid = l+(r-l)/2;
+            if(nums[mid] == target)
+            {
+                return true;
+            }
+
+            if(nums[l]==nums[mid] && nums[mid]==nums[r])
+            {
+                l++;
+                r--;
+                continue;
+            }
+
+            else if(nums[l]<=nums[mid])
+            {
+                if(nums[l]<=target && target<nums[mid])
+                {
+                    r=mid-1;
+                }
+
+                else
+                {
+                    l=mid+1;
+                }
+            }
+
+            else if(nums[mid]<=nums[r])
+            {
+                if(nums[mid]<target && target<=nums[r])
+                {
+                    l=mid+1;
+                }
+
+                else
+                {
+                    r=mid-1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
