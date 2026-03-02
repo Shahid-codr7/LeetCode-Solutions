e32cfaec6265a8115590f09a54e86800dc3a7e57
Shahid Abdul Farhad <farhadshahidabdul@gmail.com>
2026-03-03 01:28:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -0,0 +1,30 @@
+class Solution 
+{
+    public int singleNonDuplicate(int[] nums) 
+    {
+        int n=nums.length;
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
+
+        for(int i=0;i<n-1;)
+        {   
+            if(nums[i]==nums[i+1])
+                i+=2;
+            
+            else 
+                return nums[i];
+        }
+
+        return -1;
+    //     int left=0,right=n-1,mid;
+    //     while(left<=right)
+    //     {
+    //         mid=left+(right-left)/2;
+    //         if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1])
+    //         {
+    //             return nums[mid];
+    //         }
+
+    //         else if
+    //     }
+    }
+}
