a8b90cd09c11e66cfe9b669293e16b810be20ec2
Shahid Abdul Farhad <farhadshahidabdul@gmail.com>
2026-03-03 02:40:53 +0530

    Time: 0 ms (100%), Space: 52.6 MB (95.11%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -8,27 +8,25 @@ class Solution
         if(n==0) return -1;
         if(n==1) return nums[0];
         if(nums[n-1]!=nums[n-2]) return nums[n-1];
+        if(nums[0]!=nums[1]) return nums[0];
 
-        for(int i=0;i<n-1;)
-        {   
-            if(nums[i]==nums[i+1])
-                i+=2;
+        int left=0,right=n-1,mid;
+        while(left<=right)
+        {
+            mid=left+(right-left)/2;
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1])
+                return nums[mid];
+
+            else if((mid%2==0 && nums[mid]==nums[mid+1]) || (mid%2!=0 && nums[mid]==nums[mid-1]))
+                // Target is on the right side
+                left=mid+1;
+
+            else
+                // Target is on the left side
+                right=mid-1;
             
-            else 
-                return nums[i];
         }
 
         return -1;
-    //     int left=0,right=n-1,mid;
-    //     while(left<=right)
-    //     {
-    //         mid=left+(right-left)/2;
-    //         if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1])
-    //         {
-    //             return nums[mid];
-    //         }
-
-    //         else if
-    //     }
     }
 }
