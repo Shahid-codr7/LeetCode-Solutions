6691c86cbc2a09980927ee5eacd18f4a6b7fcd81
Shahid Abdul Farhad <farhadshahidabdul@gmail.com>
2026-03-03 16:24:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,31 +1,24 @@
-class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        // for(int i=0;i<matrix.length;i++){
-        //     for(int j=0;j<matrix[0].length;j++){
-        //         if(target==matrix[i][j]){
-        //             return true;
-        //         }
-        //     }
-        // }
-        // return false;
-        int m=matrix.length;
-        int n=matrix[0].length;
-        int low=0;
-        int high=m*n-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            int row=mid/n;
-            int col=mid%n;
-            if (matrix[row][col]==target){
+class Solution 
+{
+    public boolean searchMatrix(int[][] matrix, int target) 
+    {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int left=0, right=(n*m)-1, mid;
+
+        while(left<=right)
+        {
+            mid=left+(right-left)/2;
+            if(matrix[mid/m][mid%m] == target)
                 return true;
-            }
-            else if(matrix[row][col]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
+
+            else if(matrix[mid/2][mid%2] > target)
+                right=mid-1;
+
+            else
+                left=mid+1;
         }
+
         return false;
     }
 }
